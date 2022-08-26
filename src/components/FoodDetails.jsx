8b4a1377adba13e030dd3485fc14130380e4c997
Bhavesh299 <97518972+Bhavesh299@users.noreply.github.com>
2022-08-26 15:53:29 +0530

    Done with details page
    Done details page

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -64,10 +64,21 @@ export const FoodDetails = () => {
                 alt="Image"
               />
         </div>
-        <div className="map_item mt-4 md:ml-10">
+        <div className="  md:ml-10">
         {food.map((e) => {
         return (
-          <p className=" m-auto text-center " >Menu_Items: {e.Menu_Items}</p>
+          <div className="p_div">
+          <p className="map_item ">Menu_Items:- {e.Menu_Items}</p>
+          <p className="map_item ">Menu_Category:- {e.Menu_Category}</p>
+          <p className="map_item ">Per_Serve_Size:- {e.Per_Serve_Size}</p>
+          <p className="map_item ">Protein_g:- {e.Protein_g}</p>
+          <p className="map_item ">Sodium_mg:- {e.Sodium_mg}</p>
+          <p className="map_item ">Total_Sugars_g:- {e.Total_Sugars_g}</p>
+          <p className="map_item ">Total_fat_g:- {e.Total_fat_g}</p>
+          <p className="map_item ">Total_carbohydrate_g:- {e.Total_carbohydrate_g}</p>
+          
+          
+          </div>
         )
         })
       }
