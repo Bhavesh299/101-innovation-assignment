cb9b08d0960ae8cd791cbb95fdd6d981445d7cc6
Bhavesh299 <97518972+Bhavesh299@users.noreply.github.com>
2022-08-26 17:02:44 +0530

    Filter Function Done
    Filter Done

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -9,12 +9,16 @@ export const FoodList = () => {
   // console.log(Data);
 
   const [Data , setData] = useState([]) ;
+  const [Data1, SetData1] = useState([]);
+  const [sort, Setsort] = useState([]);
+  const [filter, Setfilter] = useState([]);
 
     const getData = ()=>{
       axios.get("https://run.mocky.io/v3/5ff1e1a8-b652-40c4-8c1f-5fb701ece088")
       .then((res) =>{
       const fectchData =  res.data ;
       setData(fectchData)
+      SetData1(fectchData)
       })
       .catch((e) =>{
         console.log(e) ;
@@ -27,9 +31,43 @@ export const FoodList = () => {
     const navigate = useNavigate();
     console.log(Data) ;
 
+    const HandelChangeFilter = (e)=>{
+
+      const key = e.target.value;
+      // console.log(arr)
+      Setfilter(key);
+      let arr = Data.filter((e) => {
+        return e.Menu_Category === key ;
+      });
+      SetData1(arr);
+    };
+
   return (
-    <div className="mt-5 rounded-md maindiv  text-center ">
-      {Data.map((item) => {
+    <div className="div"  >
+      <div className="flex justify-around mt-4">
+       <div className="Filterdiv">
+        <select onChange={ HandelChangeFilter}>
+        <option value="">Filter By Cetogory</option>
+        <option value="Regular Menu" > Regular Menu</option>
+        <option value="Breakfast Menu" > Breakfast Menu</option>
+        <option value="McCafe Menu" > McCafe Menu</option>
+        <option value="Gourmet Menu" >  Gourmet Menu</option>
+       
+        </select>
+        </div>
+        <div className="div2">
+        <select name="" id="">
+          <option value="Filter By Cetogory">Sort By Sugar</option>
+        <option value="Regular Menu"> High To Low</option>
+        <option value="Breakfast Menu"> Low To High</option>
+       
+       
+        </select>
+        </div>
+      
+      </div>
+      <div className="mt-5 rounded-md maindiv  text-center ">
+      {Data1.map((item) => {
         return (
           <div
           key={item.Id}
@@ -42,8 +80,9 @@ export const FoodList = () => {
               />
             </div>
             <div className=" w-72 m-3">
-              <p className="mt-6">
-                {item.Menu_Items} 
+              <p className="mt-6">Name:- {item.Menu_Items} 
+              </p>
+              <p className=""> Category:- {item.Menu_Category} 
               </p>
             </div >
             
@@ -52,12 +91,13 @@ export const FoodList = () => {
               navigate(`./FoodDetails/${item.Id}`);
             }}
             >More Details</button>
-              <button className="button">Add To favorite</button>
+              <button className="button">Add To favourite</button>
             </div>
          
           </div>
         );
       })}
+      </div>
     </div>
   );
 };
